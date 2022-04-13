443c193e846c549e212de8e4a488ddf87ee11713
Brent <brenton.crowley@gmail.com>
2022-04-13 15:32:14 +1000

    Initial Commit

---
+++ SoloSet/SoloSetApp.swift
@@ -0,0 +1,17 @@
+//
+//  SoloSetApp.swift
+//  SoloSet
+//
+//  Created by Brent on 13/4/2022.
+//
+
+import SwiftUI
+
+@main
+struct SoloSetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
